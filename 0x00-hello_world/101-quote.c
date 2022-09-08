43ba51a0e47198b62e56a8b282c433abf5c29527
kudzie-zoh <kzoraunye@gmail.com>
2022-09-08 13:14:32 -0700

    Using fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
  * main - Entry point
  *
  * Description: Print a quote using the write function
- * 
+ *
  * Return: This time we return an error 1.
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
