a99a662ae516eb45d7df8016178d54de9afd13d9
kudzie-zoh <kzoraunye@gmail.com>
2022-09-08 02:18:02 -0700

    Using Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - where the program starts
+ * program - using the put function to print to standard output
+ * return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
