d8e6411d0779bbdda0115a2c7c7283a3aa2c238e
kudzie-zoh <kzoraunye@gmail.com>
2022-09-08 13:03:09 -0700

    Using fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a quote using the write function
+ * 
+ * Return: This time we return an error 1.
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
