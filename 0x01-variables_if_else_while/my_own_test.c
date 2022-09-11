ec243fdb5a5f3d2ee8ddedc6695af5a5e87eb6e0
kudzie-zoh <kzoraunye@gmail.com>
2022-09-11 05:41:08 -0700

    My own test worked!

---
+++ 0x01-variables_if_else_while/my_own_test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: My second alternative code of 2 by 2 C Program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 < 100)
+	{
+		d2 = 1;
+		while (d2 < 100)
+		{
+			if (d2 > d1)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+				putchar(',');
+				putchar(' ');
+			}
+			++d2;
+		}
+		++d1;
+	}
+	putchar('\n');
+	return (0);
+}
