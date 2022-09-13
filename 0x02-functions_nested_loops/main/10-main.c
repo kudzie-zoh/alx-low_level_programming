b95c24f2b95661e0d89adce64637137b701218f9
kudzie-zoh <kzoraunye@gmail.com>
2022-09-13 10:31:29 -0700

    The last batch of nested loops

---
+++ 0x02-functions_nested_loops/main/10-main.c
@@ -0,0 +1,19 @@
+#include "../main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: check code for sum
+ *
+ * Return: Always 0 (Succcess)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main/11-main.c
@@ -0,0 +1,19 @@
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Check code for count to 98
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
