c6d4a3f80eab4faf9962ba94308c095604c96fc9
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-09-19 09:06:11 +0200

    Create 0-main.c

---
+++ 0x05-pointers_arrays_strings/main/0-main.c
@@ -0,0 +1,23 @@
+#include "../main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n = %d\n", n);
+	reset_to_98(&n);
+	printf("n = %d\n", n);
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
