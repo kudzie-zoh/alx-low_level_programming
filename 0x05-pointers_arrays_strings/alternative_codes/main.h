71a8add586cc937bd9f518d708aecea001ae0c14
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-09-28 18:46:22 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/alternative_codes/main.h
+++ 0x05-pointers_arrays_strings/alternative_codes/main.h
@@ -8,6 +8,8 @@ int _strcmp(char *s1, char *s2);
 void time_table(int n, int m);
 void print_diagsums(int *a, int size);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int find_sqr(int i, int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
