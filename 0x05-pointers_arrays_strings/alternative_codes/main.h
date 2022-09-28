0be97e7e3608a7ba92f75a3a1902d622264e4aa2
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-09-28 10:13:07 +0200

    update on diagonal add prototype

--- 0x05-pointers_arrays_strings/alternative_codes/main.h
+++ 0x05-pointers_arrays_strings/alternative_codes/main.h
@@ -6,6 +6,7 @@
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void time_table(int n, int m);
+void print_diagsums(int *a, int size);
 int _putchar(char c);
 
 #endif /* MAIN_H */
