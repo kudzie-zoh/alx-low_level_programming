35812da3b627eeadba8177e4a0142069ea789fe0
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-10-05 14:13:38 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
