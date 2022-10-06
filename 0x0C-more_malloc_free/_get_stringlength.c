fce3e7901d7db94b5b26a905d2417f7a09d0091f
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-10-06 11:41:01 +0200

    Create _get_stringlength.c

---
+++ 0x0C-more_malloc_free/_get_stringlength.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _length - get the length of strings
+ *
+ * @str: string to get length of
+ *
+ * Return: length of string
+*/
+
+int _length(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
