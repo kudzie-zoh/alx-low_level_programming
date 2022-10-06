9dc95223dc978acf167d5e0af88b7dfb44d54431
Kudzai Zoraunye <kzoraunye@gmail.com>
2022-10-06 12:11:56 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -94,7 +94,24 @@ int _length(char *str)
 	return (i);
 }
 
+/**
+ * error_exit - prints error with _putchar
+ *              and exits with 98
+ *
+ * Return: Error 98 and exit(98)
+*/
 
+int error_exit(void)
+{
+	char *err;
+	int i;
+
+	err = "Error";
+	for (i = 0; err[i] != '\0'; i++)
+		_putchar(err[i]);
+	_putchar('\n');
+	exit(98);
+}
 
 void multiply(char *s1, char *s2)
 {
