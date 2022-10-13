0b0f4ea36e161181e19d0bfad00f37c6e1eae704
kudzie-zoh <kzoraunye@gmail.com>
2022-10-13 00:46:06 -0700

    Variadic Functions

--- 0x10-variadic_functions/main/3-main.c
+++ 0x10-variadic_functions/main/3-main.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-	print_all("ceis", 'H', 0, "lberton");
-	print_all("fcsi", 3.14435, 'H', NULL, 402);
+	print_all("ceis", 'B', 3, "stSchool");
 	return (0);
 }
---
+++ 0x10-variadic_functions/main/README.md
@@ -0,0 +1,1 @@
+Test files for the task provided by ALX
